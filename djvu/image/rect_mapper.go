d940c3185c91fb6b50f5b2a8be67911094ce5096
Jan Reggie Dela Cruz <janreggie.delacruz@gmail.com>
2022-03-29 19:39:51 +0800

    +image.RectMapper implementation

--- djvu/image/rect_mapper.go
+++ djvu/image/rect_mapper.go
@@ -28,13 +28,181 @@ func NewRectMapper(src Rect, dst Rect) (*RectMapper, error) {
 		return nil, errors.New("either src or dst is empty")
 	}
 
-	return &RectMapper{
-		src:  src,
-		dst:  dst,
-		code: 0,
-		rw:   newRatio(dst.Width(), src.Width()),
-		rh:   newRatio(dst.Height(), src.Height()),
-	}, nil
+	rm := &RectMapper{
+		src: src,
+		dst: dst,
+	}
+	rm.precalc()
+	return rm, nil
+}
+
+// Set sets the source and destination rectangles
+func (rm *RectMapper) Set(src Rect, dst Rect) error {
+	if src.IsEmpty() || dst.IsEmpty() {
+		return errors.New("either src or dst is empty")
+	}
+
+	rm.src = src
+	rm.dst = dst
+
+	if rm.code&_SWAPXY != 0 {
+		rm.src.xmin, rm.src.ymin = rm.src.ymin, rm.src.xmin
+		rm.src.xmax, rm.src.ymax = rm.src.ymax, rm.src.xmax
+	}
+
+	rm.precalc()
+	return nil
+}
+
+// precalc calculates rw and rh when changing/updating the src/dst rectangles
+func (rm *RectMapper) precalc() {
+	rm.rw = newRatio(rm.dst.Width(), rm.src.Width())
+	rm.rh = newRatio(rm.dst.Height(), rm.src.Height())
+}
+
+// Get gets the source and destination rectangles
+func (rm *RectMapper) Get() (src, dst Rect) {
+	src = rm.src
+	dst = rm.dst
+	return
+}
+
+// Rotate composes the affine transform
+// with a rotation of `turns` quarter turns *counter-clockwise*.
+func (rm *RectMapper) Rotate(turns int) {
+	oldcode := rm.code
+	switch turns & 0x3 {
+	case 1:
+		if rm.hasFlag(_SWAPXY) {
+			rm.code ^= _MIRRORY
+		} else {
+			rm.code ^= _MIRRORX
+		}
+		rm.code ^= _SWAPXY
+
+	case 2:
+		rm.code ^= (_MIRRORX | _MIRRORY)
+
+	case 3:
+		if rm.hasFlag(_SWAPXY) {
+			rm.code ^= _MIRRORX
+		} else {
+			rm.code ^= _MIRRORY
+		}
+		rm.code ^= _SWAPXY
+	}
+
+	if (oldcode^rm.code)&_SWAPXY != 0 {
+		rm.src.xmin, rm.src.ymin = rm.src.ymin, rm.src.xmin
+		rm.src.xmax, rm.src.ymax = rm.src.ymax, rm.src.xmax
+	}
+}
+
+// MirrorX composes the affine transform with a symmetry
+// with respect to the vertical line
+// crossing the center of the output rectangle.
+func (rm *RectMapper) MirrorX() {
+	rm.code ^= _MIRRORX
+}
+
+// MirrorX composes the affine transform with a symmetry
+// with respect to the horizontal line
+// crossing the center of the output rectangle.
+func (rm *RectMapper) MirrorY() {
+	rm.code ^= _MIRRORY
+}
+
+// Map maps a point according to the affine transform.
+// (x,y) represents the coordinates of a point.
+// This method returns the coordinates of a second point
+// located in the same position relative to the corners of the output rectangle
+// as the first point relative to the matching corners of the input rectangle.
+// Coordinates are rounded to the nearest integer.
+func (rm *RectMapper) Map(x, y int32) (int32, int32) {
+	x2, y2 := x, y
+
+	// Swap and mirror
+	if rm.hasFlag(_SWAPXY) {
+		x2, y2 = y2, x2
+	}
+	if rm.hasFlag(_MIRRORX) {
+		x2 = rm.src.xmin + rm.src.xmax - x2
+	}
+	if rm.hasFlag(_MIRRORY) {
+		y2 = rm.src.ymin + rm.src.ymax - y2
+	}
+
+	// Scale and translate
+	x2 = rm.dst.xmin + rmul(x2-rm.src.xmin, rm.rw)
+	y2 = rm.dst.ymin + rmul(y2-rm.src.ymin, rm.rh)
+
+	return x2, y2
+}
+
+// MapRect returns a Rectangle
+// whose points are mapped according to the affine transform.
+func (rm *RectMapper) MapRect(r Rect) Rect {
+	xmin, ymin := rm.Map(r.xmin, r.ymin)
+	xmax, ymax := rm.Map(r.xmax, r.ymax)
+	if xmin > xmax {
+		xmin, xmax = xmax, xmin
+	}
+	if ymin > ymax {
+		ymin, ymax = ymax, ymin
+	}
+
+	return Rect{
+		xmin: xmin,
+		ymin: ymin,
+		xmax: xmax,
+		ymax: ymax,
+	}
+}
+
+// Unmap maps a point according to the inverse of the affine transform.
+// That is, x1,y1 = rm.Unmap(x2,y2) <-> x2,y2 = rm.Map(x1,y1).
+func (rm *RectMapper) Unmap(x, y int32) (int32, int32) {
+	// Scale and translate
+	x2 := rm.src.xmin + rdiv(x-rm.dst.xmin, rm.rw)
+	y2 := rm.src.ymin + rdiv(y-rm.dst.ymin, rm.rh)
+
+	// Mirror and swap
+	if rm.hasFlag(_MIRRORX) {
+		x2 = rm.src.xmin + rm.src.xmax - x2
+	}
+	if rm.hasFlag(_MIRRORY) {
+		y2 = rm.src.ymin + rm.src.ymax - y2
+	}
+	if rm.hasFlag(_SWAPXY) {
+		x2, y2 = y2, x2
+	}
+
+	return x2, y2
+}
+
+// UnmapRect maps a Rectangle according to the inverse of the affine transform.
+// That is, r1 = rm.UnmapRect(r2) <-> r2 = rm.MapRect(r1).
+func (rm *RectMapper) UnmapRect(r Rect) Rect {
+	xmin, ymin := rm.Unmap(r.xmin, r.ymin)
+	xmax, ymax := rm.Unmap(r.xmax, r.ymax)
+	if xmin > xmax {
+		xmin, xmax = xmax, xmin
+	}
+	if ymin > ymax {
+		ymin, ymax = ymax, ymin
+	}
+
+	return Rect{
+		xmin: xmin,
+		ymin: ymin,
+		xmax: xmax,
+		ymax: ymax,
+	}
+}
+
+// hasFlag checks if rm.code has some flag set
+func (rm *RectMapper) hasFlag(f byte) bool {
+	return rm.code&f != 0
 }
 
 // Some codes for RectMapper
